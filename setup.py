056fd961f7c1edbf4ac2f4dec13978d07526bfe5
sanjaydatasciencedojo <sanjay@datasciencedojo.com>
2019-12-13 16:26:31 -0800

    update file

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
 setuptools.setup(
-    name="jupyter-rsession-proxy",
+    name="jupyter-testrsession-proxy",
     version='1.0dev',
-    url="https://github.com/jupyterhub/jupyter-rsession-proxy",
+    url="https://github.com/sanjaydatasciencedojo/jupyter-rsession-proxy",
     author="Ryan Lovett & Yuvi Panda",
     description="Jupyter extension to proxy RStudio's rsession",
     packages=setuptools.find_packages(),
@@ -14,8 +14,7 @@ setuptools.setup(
     ],
     entry_points={
         'jupyter_serverproxy_servers': [
-            'rstudio = jupyter_rsession_proxy:setup_rstudio',
-            'shiny = jupyter_rsession_proxy:setup_shiny'
+            'shiny = jupyter_rsession_proxy:setup_testshiny'
         ]
     },
     package_data={
